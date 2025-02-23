4a8ba7d905cd7b622a3b6bd3c396af60bd5ace6b
JSRobotics <sjohnsen.valle@gmail.com>
2025-02-23 18:17:44 +0100

    Working on "thread" safety between tasks.

--- src/tasks/main_task.c
+++ src/tasks/main_task.c
@@ -1,10 +1,18 @@
 //Created by Bananums: https://github.com/Bananums
 
 #include "main_task.h"
+#include "integration/task_data.h"
 
 void main_task(void *arg) {
-    const MainTaskParams params = *(MainTaskParams *) arg;
-    QueueHandle_t queue = params.queue;
+    const MainTaskParams *params = (MainTaskParams *) arg;
+    QueueHandle_t queue = params->queue;
+    SharedState *shared_state = params->shared_state;
+    DrivePayload drive_payload = {
+    .heartbeat = 0,
+    .steering = 0.0f,
+    .throttle = 0.0f,
+    };
+
     uint8_t byte = 0;
     while (1) {
         if (byte == 255) {
@@ -12,7 +20,11 @@ void main_task(void *arg) {
         }
         byte++;
 
-        if (xQueueSend(queue, &byte, sizeof(byte)) == pdTRUE) {
+        ReadDriveState(shared_state, &drive_payload);
+        float steering = drive_payload.steering;
+        printf("Got drive.steering %f.2\n", steering);
+
+        if (xQueueSend(queue, &byte, portMAX_DELAY) == pdTRUE) {  //TODO look into portMAX_DELAY
             printf("Sending to queue: byte %d\n", byte);
         } else {
             printf("Failed to send to queue: byte %d\n", byte);
